59169abbfdb758b75c9bdb5f95d3ed640a50bc2b
Erdi Rowlands <erdi.rowlands@harness.io>
2022-09-14 17:57:22 +0100

    FFM-4277 Rearrange test case

--- src/cfclient_cache_repository_test.erl
+++ src/cfclient_cache_repository_test.erl
@@ -5,16 +5,18 @@
 
 get_flag_and_cache_test() ->
   PID = self(),
+
+  %% Key is found in cache
+  meck:new(lru),
+  meck:expect(lru, get,  fun(PID, "flags/does_exist") -> "flags/does_exist" end),
+  ?assertEqual("flags/does_exist", cfclient_cache_repository:get_flag_and_cache(PID, "does_exist")),
+  meck:unload(lru),
+
   %% Key is not found in cache
   meck:new(lru),
   meck:expect(lru, get,  fun(PID, "flags/does_not_exist") -> undefined end),
   ?assertEqual(undefined, cfclient_cache_repository:get_flag_and_cache(PID, "does_not_exist")),
-  meck:unload(lru),
-
-  %% Key is found in cache
-  %% Key is not found in cache
-  meck:new(lru),
-  meck:expect(lru, get,  fun(PID, "flags/does_exist") -> "flags/does_exist" end),
-  ?assertEqual("flags/does_exist", cfclient_cache_repository:get_flag_and_cache(PID, "does_exist")),
   meck:unload(lru).
+
+
 
