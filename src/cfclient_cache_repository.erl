4f87d0def749664f99c8d0055e2175964e811510
Erdi Rowlands <erdi.rowlands@harness.io>
2022-09-15 12:24:33 +0100

    FFM-4277 Comment

--- src/cfclient_cache_repository.erl
+++ src/cfclient_cache_repository.erl
@@ -4,16 +4,17 @@
 
 %% @doc Get flag value from cache
 %% @end
+
+-spec get_flag_and_cache(cfapi_feature_config1:cfapi_feature_config(), any()) -> any().
 get_flag_and_cache(CachePID, Identifier) ->
   FlagKey = format_flag_key(Identifier),
   Flag = lru:get(CachePID, FlagKey),
   if
     Flag /= undefined->
       Flag;
-    %% TODO need to return an OAPI generated model for FeatureConfig once OAPI is integrated.
+    %% TODO returning undefined for now - but we need to figure out the errors to return.
     true ->
-    %%
-    undefined
+      undefined
 
 end.
 
