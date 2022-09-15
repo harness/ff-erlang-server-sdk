cf7ed907062e40e5898a785bc5e249a98ca0ac67
Erdi Rowlands <erdi.rowlands@harness.io>
2022-09-15 16:03:56 +0100

    FFM-4277 Quick backup before using pattern matching

--- src/cfclient_cache_repository.erl
+++ src/cfclient_cache_repository.erl
@@ -1,6 +1,11 @@
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% LRU Repository for Target and Segment configuration
+%%% @end
+%%%-------------------------------------------------------------------
 -module(cfclient_cache_repository).
 
--export([get_flag_and_cache/2]).
+-export([get_flag_and_cache/2, get_segment_and_cache/2]).
 
 %% @doc Get flag value from cache
 %% @end
@@ -8,15 +13,23 @@
 -spec get_flag_and_cache(CachePID :: pid(), Identifier :: string()) -> string().
 get_flag_and_cache(CachePID, Identifier) ->
   FlagKey = format_flag_key(Identifier),
+  get_from_cache(CachePID, FlagKey).
+
+-spec get_segment_and_cache(CachePID :: pid(), Identifier :: string()) -> string().
+get_segment_and_cache(CachePID, Identifier) ->
+  FlagKey = format_flag_key(Identifier),
+  get_from_cache(CachePID, FlagKey).
+
+-spec get_from_cache(CachePID :: pid(), Identifier :: string()) -> term().
+get_from_cache(CachePID, FlagKey) ->
   Flag = lru:get(CachePID, FlagKey),
   if
-    Flag /= undefined->
+    Flag /= undefined ->
       Flag;
-    %% TODO returning undefined for now - but we need to figure out the errors to return.
+  %% TODO returning undefined for now - but we need to figure out the errors to return.
     true ->
       undefined
-
-end.
+  end.
 
 -spec format_flag_key(string()) -> string().
 format_flag_key(Identifier) ->
