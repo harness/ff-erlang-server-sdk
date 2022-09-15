cd53d08892a6092e2ce30b5c0f529b32157034fe
Erdi Rowlands <erdi.rowlands@harness.io>
2022-09-15 11:55:48 +0100

    FFM-4277 Tidy up and make some public functions private

--- src/cfclient_cache_repository.erl
+++ src/cfclient_cache_repository.erl
@@ -1,24 +1,69 @@
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% LRU Repository for Flag and Segment configuration
+%%% @end
+%%%-------------------------------------------------------------------
 -module(cfclient_cache_repository).
 
--export([get_flag_and_cache/2]).
+-export([get_from_cache/2, set_to_cache/3]).
 
-%% @doc Get flag value from cache
+-type flag() :: {flag, Identifier :: string()}.
+-type segment() :: {segment, Identifier :: string()}.
+
+%% @doc Get a flag or segment from the cache.
 %% @end
-get_flag_and_cache(CachePID, Identifier) ->
-  FlagKey = format_flag_key(Identifier),
+-spec get_from_cache(flag() | segment(), CachePID :: pid()) -> cfapi_feature_config:cfapi_feature_config() | cfapi_segment:cfapi_segment().
+get_from_cache({Type, Identifier}, CachePID) ->
+  FlagKey = format_key({Type, Identifier}),
+  get(CachePID, FlagKey).
+
+-spec get(CachePID :: pid(), Identifier :: string()) -> term().
+get(CachePID, FlagKey) ->
   Flag = lru:get(CachePID, FlagKey),
   if
-    Flag /= undefined->
+    Flag /= undefined ->
       Flag;
-    %% TODO need to return an OAPI generated model for FeatureConfig once OAPI is integrated.
+  %% @TODO returning undefined for now - but we need to figure out if this repository should return errors or not - or just let the caller handle it.
     true ->
-    %%
-    undefined
+      undefined
+  end.
 
-end.
+%% @doc Places a flag or segment into the cache with the new value
+%% @end
+%% @TODO - relies on cfapi_feature_config type
+-spec set_to_cache(flag() | segment(), cfapi_feature_config:cfapi_feature_config() | cfapi_segment:cfapi_segment() , CachePID :: pid()) -> atom().
+set_to_cache({flag, Identifier}, Feature,  CachePID) ->
+  IsOutdated = is_outdated({flag, Identifier}, Feature, CachePID),
+  set(CachePID, Identifier, Feature, IsOutdated).
 
-format_flag_key(Identifier) ->
-  "flags/" ++ Identifier.
+-spec set(CachePID :: pid(), Identifier :: string(), Value :: cfapi_feature_config:cfapi_feature_config() | cfapi_segment:cfapi_segment(),  Outdated :: boolean()) -> atom().
+set(CachePID, Identifier, Value, true) ->
+  lru:add(CachePID, Identifier, Value),
+  logger:debug("The flag is outdated"),
+  ok;
+set(_, _, _, false) ->
+  logger:debug("The flag is outdated"),
+  not_ok.
 
-format_segment_key(Identifier) ->
+%%%%%% @TODO - relies on cfapi_feature_config type
+-spec is_outdated(flag() | segment(),cfapi_feature_config:cfapi_feature_config() | cfapi_segment:cfapi_segment(), CachePID :: pid()) -> boolean().
+is_outdated({flag, Identifier}, Feature, CachePID) ->
+  OldFeature = get_from_cache({flag, Identifier}, CachePID),
+  #{version := OldFeatureVersion} = OldFeature,
+  #{version := NewFeatureVersion} = Feature,
+  OldFeatureVersion > NewFeatureVersion;
+is_outdated({segment, Identifier}, Segment, CachePID) ->
+  OldSegment = get_from_cache({segment, Identifier}, CachePID),
+  #{version := OldSegmentVersion} = OldSegment,
+  #{version := NewSegmentVersion} = Segment,
+  OldSegmentVersion > NewSegmentVersion.
+
+-spec format_key(flag() | segment()) -> string().
+format_key({flag, Identifier}) ->
+  "flags/" ++ Identifier;
+format_key({segment, Identifier}) ->
   "segments/" ++ Identifier.
+
+
+
+
--- src/cfclient_cache_repository_test.erl
+++ src/cfclient_cache_repository_test.erl
@@ -3,20 +3,46 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-get_flag_and_cache_test() ->
+%% TODO - used so we can actually use the debugger as there is a default timeout of 5 seconds
+%% for eunit tests. This is a temporary workaround and should be replaced with a better solution as
+%% as it slows down tests when debugging isn't needed.
+ slow_test_() ->
+           {timeout, 60,
+          fun() ->
+                  timer:sleep(10000)
+           end}.
+
+get_from_cache_test() ->
   PID = self(),
 
-  %% Key is found in cache
+  %% Flag is found in cache
   meck:new(lru),
   meck:expect(lru, get,  fun(PID, "flags/does_exist") -> "flags/does_exist" end),
-  ?assertEqual("flags/does_exist", cfclient_cache_repository:get_flag_and_cache(PID, "does_exist")),
+  ?assertEqual("flags/does_exist", cfclient_cache_repository:get_from_cache({flag, "does_exist"}, PID)),
   meck:unload(lru),
 
-  %% Key is not found in cache
+  %% Flag is not found in cache
   meck:new(lru),
   meck:expect(lru, get,  fun(PID, "flags/does_not_exist") -> undefined end),
-  ?assertEqual(undefined, cfclient_cache_repository:get_flag_and_cache(PID, "does_not_exist")),
+  ?assertEqual(undefined, cfclient_cache_repository:get_from_cache({flag, "does_not_exist"}, PID)),
+  meck:unload(lru),
+
+  %% Segment is found in cache
+  meck:new(lru),
+  meck:expect(lru, get,  fun(PID, "segments/does_exist") -> "segments/does_exist" end),
+  ?assertEqual("segments/does_exist", cfclient_cache_repository:get_from_cache({segment, "does_exist"}, PID)),
+  meck:unload(lru),
+
+  %% Segment is not found in cache
+  meck:new(lru),
+  meck:expect(lru, get,  fun(PID, "segments/does_not_exist") -> undefined end),
+  ?assertEqual(undefined, cfclient_cache_repository:get_from_cache({segment, "does_not_exist"}, PID)),
   meck:unload(lru).
 
+format_key_test() ->
+  {timeout,3600},
+  %% Flag key
+  ?assertEqual("flags/flag_1", cfclient_cache_repository:format_key({flag, "flag_1"})),
 
-
+  %% Segment key
+  ?assertEqual("segments/segment_1", cfclient_cache_repository:format_key({segment, "segment_1"})).
