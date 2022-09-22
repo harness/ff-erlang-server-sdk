9e1739eb9d173595a0d0ea9943007369a6133846
Erdi Rowlands <erdi.rowlands@harness.io>
2022-09-22 20:31:10 +0100

    FFM-4278 Hacky mess but getting 200 on feature-configs. Needs tidied up

--- src/cfclient_retrieve.erl
+++ src/cfclient_retrieve.erl
@@ -5,7 +5,7 @@
 %%%-------------------------------------------------------------------
 -module(cfclient_retrieve).
 
--export([retrieve_flags/0]).
+-export([retrieve_flags/0, authenticate/0).
 
 
 retrieve_flags() ->
@@ -14,9 +14,18 @@ retrieve_flags() ->
 
   %% TODO Should be parameterized - probably coming in as a string? If so, will need list_to_binary. Using binary for this hard code.
   EnvironmentID = <<"608a206a-f497-4210-b66d-15c6182c0dfb">>,
-  BearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZpcm9ubWVudCI6IjYwOGEyMDZhLWY0OTctNDIxMC1iNjZkLTE1YzYxODJjMGRmYiIsImVudmlyb25tZW50SWRlbnRpZmllciI6ImJyeWFuIiwicHJvamVjdCI6ImQ1MTU3MzNjLTIzMzctNDk0Mi04NWY2LTZjMTY2MTE4MTI1YSIsInByb2plY3RJZGVudGlmaWVyIjoiQnJ5YW5fSmVuIiwiYWNjb3VudElEIjoiYTVtQW5oQmpRT0tieGpVSFJfcjNRdyIsIm9yZ2FuaXphdGlvbiI6IjhiMjQxOWMzLWE1Y2YtNGMzMS1iZmYxLWEzZTFiNTJmMjdkYiIsIm9yZ2FuaXphdGlvbklkZW50aWZpZXIiOiJkZWZhdWx0IiwiY2x1c3RlcklkZW50aWZpZXIiOiIyIiwia2V5X3R5cGUiOiJTZXJ2ZXIifQ.yonipuhfW233lOMBm_b5nkxi7b7kMfbR572GXHKu3oo",
-  Optional = #{cfg => #{auth => #{"apiKey" => BearerToken}, host => "https://config.ff.harness.io/api/1.0"}},
+  BearerToken = <<"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZpcm9ubWVudCI6IjYwOGEyMDZhLWY0OTctNDIxMC1iNjZkLTE1YzYxODJjMGRmYiIsImVudmlyb25tZW50SWRlbnRpZmllciI6ImJyeWFuIiwicHJvamVjdCI6ImQ1MTU3MzNjLTIzMzctNDk0Mi04NWY2LTZjMTY2MTE4MTI1YSIsInByb2plY3RJZGVudGlmaWVyIjoiQnJ5YW5fSmVuIiwiYWNjb3VudElEIjoiYTVtQW5oQmpRT0tieGpVSFJfcjNRdyIsIm9yZ2FuaXphdGlvbiI6IjhiMjQxOWMzLWE1Y2YtNGMzMS1iZmYxLWEzZTFiNTJmMjdkYiIsIm9yZ2FuaXphdGlvbklkZW50aWZpZXIiOiJkZWZhdWx0IiwiY2x1c3RlcklkZW50aWZpZXIiOiIyIiwia2V5X3R5cGUiOiJTZXJ2ZXIifQ.yonipuhfW233lOMBm_b5nkxi7b7kMfbR572GXHKu3oo">>,
+  %%Optional = #{cfg => #{ host => "https://config.ff.harness.io"}, auth => #{ "BearerAuth" => #{type => http, key => BearerToken, in => header }},  params => #{cluster => "2" }},
+  Optional = #{ cfg => #{auth => #{ 'BearerAuth' => BearerToken}, host => "https://config.ff.harness.io"},  params => #{cluster => "2" }},
   cfapi_client_api:get_feature_config(Context, EnvironmentID, Optional).
 
 
+
+authenticate() ->
+   Ctx = ctx:new(),
+   Opts = #{ cfg => #{host => "https://config.ff.harness.io"}, params => #{ apiKey => 'f5a86708-b483-4b06-a048-3690664b1783', target => #{ identifier => 'erlang', name => 'erlang' }}},
+   {Status, ResponseBody, Headers} = cfapi_client_api:authenticate(Ctx, Opts),
+   AuthToken = maps:get('authToken', ResponseBody),
+   io:format("~p~n~n", [AuthToken]),
+   AuthToken.
 
