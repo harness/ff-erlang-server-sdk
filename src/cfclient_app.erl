f2f9f68c4184b791d4b68df0c0c36ac79fc02cda
Erdi Rowlands <erdi.rowlands@harness.io>
2022-09-22 12:06:58 +0100

    FFM-4278 Add OAPI as a project dependency

--- src/cfclient_app.erl
+++ src/cfclient_app.erl
@@ -7,13 +7,11 @@
 
 -behaviour(application).
 
--export([start/2, stop/1]).
+-export([start/2]).
 
 start(_StartType, _StartArgs) ->
-    ok = cfclient_config:init(),
     cfclient_sup:start_link().
 
-stop(_State) ->
-    cfclient:destroy_all.
+
 
 %% internal functions
--- src/cfclient_retrieve.erl
+++ src/cfclient_retrieve.erl
@@ -16,7 +16,7 @@ retrieve_flags() ->
   EnvironmentID = <<"608a206a-f497-4210-b66d-15c6182c0dfb">>,
   BearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZpcm9ubWVudCI6IjYwOGEyMDZhLWY0OTctNDIxMC1iNjZkLTE1YzYxODJjMGRmYiIsImVudmlyb25tZW50SWRlbnRpZmllciI6ImJyeWFuIiwicHJvamVjdCI6ImQ1MTU3MzNjLTIzMzctNDk0Mi04NWY2LTZjMTY2MTE4MTI1YSIsInByb2plY3RJZGVudGlmaWVyIjoiQnJ5YW5fSmVuIiwiYWNjb3VudElEIjoiYTVtQW5oQmpRT0tieGpVSFJfcjNRdyIsIm9yZ2FuaXphdGlvbiI6IjhiMjQxOWMzLWE1Y2YtNGMzMS1iZmYxLWEzZTFiNTJmMjdkYiIsIm9yZ2FuaXphdGlvbklkZW50aWZpZXIiOiJkZWZhdWx0IiwiY2x1c3RlcklkZW50aWZpZXIiOiIyIiwia2V5X3R5cGUiOiJTZXJ2ZXIifQ.yonipuhfW233lOMBm_b5nkxi7b7kMfbR572GXHKu3oo",
   Optional = #{cfg => #{auth => #{"apiKey" => BearerToken}, host => "https://config.ff.harness.io/api/1.0"}},
-  FeatureConfig = cfapi_client_api:get_feature_config(Context, EnvironmentID, Optional).
+  cfapi_client_api:get_feature_config(Context, EnvironmentID, Optional).
 
 
 
