bcc8091d1aa2581580eb307c717048c4145075aa
Erdi Rowlands <erdi.rowlands@harness.io>
2022-10-06 17:16:19 +0100

    FFM-4601 Default on variation tests for bool and string

--- src/cfclient_evaluator_test.erl
+++ src/cfclient_evaluator_test.erl
@@ -73,6 +73,15 @@ variations_test() ->
   %% Target included
   ?assertEqual(false, cfclient_evaluator:bool_variation(<<"My_boolean_flag">>, TargetIncludedFromGroup, false)),
 
+  %%%%%%%% Flag is on - no targets or groups %%%%%%%%
+  %% Default on variation
+  meck:expect(lru, get, fun
+                          (CacheName, <<"segments/target_group_1">>) -> target_group_no_custom_rules();
+                          (CacheName, <<"flags/My_boolean_flag">>) -> boolean_flag_no_targets_or_groups()
+                        end),
+  ?assertEqual(true, cfclient_evaluator:bool_variation(<<"My_boolean_flag">>, TargetExcludedFromGroup, false)),
+
+
   %%-------------------- String Variation --------------------
   %%%%%%%% Flag is off %%%%%%%%
   meck:expect(lru, get, fun(CacheName, <<"flags/My_string_flag">>) -> string_flag_off() end),
@@ -93,6 +102,14 @@ variations_test() ->
 
   %% Target included
   ?assertEqual("Dont_serve_it", cfclient_evaluator:string_variation(<<"My_string_flag">>, TargetIncludedFromGroup, "some_fake_default_value")),
+
+  %%%%%%%% Flag is on - no targets or groups %%%%%%%%
+  %% Default on variation
+  meck:expect(lru, get, fun
+                          (CacheName, <<"segments/target_group_1">>) -> target_group_no_custom_rules();
+                          (CacheName, <<"flags/My_boolean_flag">>) -> string_flag_no_targets_or_groups()
+                        end),
+  ?assertEqual("Serve_it", cfclient_evaluator:string_variation(<<"My_boolean_flag">>, ExistingTargetA, false)),
   meck:unload(lru).
 
 
@@ -251,67 +268,6 @@ search_targets_test() ->
 
   %% NotFound %%
   ?assertEqual(not_found, cfclient_evaluator:search_targets(<<"target_identifier_34214awe">>, TargetsMultiple)).
-
-
-search_rules_test() ->
-  TargetGroup = [#{environment => <<"dev">>, identifier => <<"target_group_2">>,
-    name => <<"target_group_2">>, version => 1},
-    #{environment => <<"dev">>, identifier => <<"target_group_1">>,
-      included =>
-      [#{account => <<>>, environment => <<>>,
-        identifier => <<"target_identifier">>,
-        name => <<"target_test">>, org => <<>>, project => <<>>}],
-      name => <<"target_group_1">>, version => 3}],
-
-  TargetGroup2 = [#{environment => <<"dev">>,
-    excluded =>
-    [#{account => <<>>, environment => <<>>,
-      identifier => <<"target_identifier">>,
-      name => <<"target_test">>, org => <<>>, project => <<>>}],
-    identifier => <<"target_group_1">>,
-    included =>
-    [#{account => <<>>, environment => <<>>,
-      identifier => <<"target_test_2">>,
-      name => <<"target_test_2">>, org => <<>>, project => <<>>}],
-    name => <<"target_group_1">>,
-    rules =>
-    [#{attribute => <<"identifier">>,
-      id => <<"493945ee-b37b-466d-900e-846a24c93bec">>,
-      negate => false, op => <<"ends_with">>,
-      values => [<<"1">>]},
-      #{attribute => <<"identifier">>,
-        id => <<"7f779368-036c-40e3-a8b7-8b69bd809f39">>,
-        negate => false, op => <<"ends_with">>,
-        values => [<<"2">>]},
-      #{attribute => <<"identifier">>,
-        id => <<"06bcb37b-111b-41c2-805a-d232e5e3dd11">>,
-        negate => false, op => <<"equal">>,
-        values => [<<"target_test_3">>]}],
-    version => 10}],
-
-  Rules = [
-    #{clauses => [#{attribute => <<>>,
-      id => <<"48b71de2-bf37-472a-ad53-b6f3cad8094e">>,
-      negate => false, op => <<"segmentMatch">>,
-      values => [<<"target_group_1">>]}],
-      priority => 1,
-      ruleId => <<"2604534c-a88f-4d00-90c6-550d1ca5f66e">>,
-      serve => #{variation => <<"true">>}},
-    #{clauses =>
-    [#{attribute => <<>>,
-      id => <<"7ad0a50e-bed6-4826-be66-66b65a132ec2">>,
-      negate => false, op => <<"segmentMatch">>,
-      values => [<<"target_group_2">>]}],
-      priority => 0,
-      ruleId => <<"403a6ac7-29ce-44d3-bfc4-5fd38c8fb317">>,
-      serve => #{variation => <<"false">>}}],
-
-  SortedList = lists:sort(
-    fun(A, B) ->
-      maps:get(priority, A) =< maps:get(priority, B)
-    end, Rules),
-
-  io:fwrite("THIS IS IT: ~p~n~n  ", [SortedList]). %% TODO debug statement - remove.
 
 
 is_rule_included_or_excluded_test() ->
@@ -501,13 +457,8 @@ string_flag_no_targets_or_groups() ->
     environment => <<"dev">>,feature => <<"My_string_flag">>,
     kind => <<"string">>,offVariation => <<"Dont_serve_it">>,
     prerequisites => [],project => <<"erlangsdktest">>,
-    rules => [],state => <<"off">>,variationToTargetMap => null,
-    variations =>
-    [#{identifier => <<"Serve_it">>,name => <<"Serve it">>,
-      value => <<"serve it">>},
-      #{identifier => <<"Dont_serve_it">>,
-        name => <<"Don't serve it ">>,
-        value => <<"don't serve it">>}],
+    rules => [],state => <<"on">>,variationToTargetMap => null,
+    variations => null,
     version => 2}.
 
 string_flag_target_and_groups() ->
@@ -543,7 +494,7 @@ string_flag_only_target() ->
     environment => <<"dev">>,feature => <<"My_string_flag">>,
     kind => <<"string">>,offVariation => <<"Dont_serve_it">>,
     prerequisites => [],project => <<"erlangsdktest">>,
-    rules => null,
+    rules => [],
     state => <<"on">>,
     variationToTargetMap =>
     [#{targets =>
