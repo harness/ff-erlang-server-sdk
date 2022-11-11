532d0fcd61f919239982b0cf493c8ac0ed89e8f6
Erdi Rowlands <erdi.rowlands@harness.io>
2022-11-11 11:42:10 +0000

    FFM-4688 Building out the operators

--- src/cfclient_evaluator.erl
+++ src/cfclient_evaluator.erl
@@ -229,14 +229,18 @@ search_group_custom_rules(_, []) -> false.
 
 
 -spec is_custom_rule_match(Operator :: atom(), TargetAttribute :: binary(), RuleValue :: binary()) -> true | false.
+%% Equal sensitive
 is_custom_rule_match(?EQUAL_SENSITIVE_OPERATOR, TargetAttribute, RuleValue) ->
   string:equal(TargetAttribute, RuleValue, false);
+%% Equal
 is_custom_rule_match(?EQUAL_OPERATOR, TargetAttribute, RuleValue) ->
   string:equal(TargetAttribute, RuleValue, true);
+%% Starts with
 is_custom_rule_match(?STARTS_WITH_OPERATOR, TargetAttribute, RuleValue) ->
   string:find(TargetAttribute, RuleValue) =:= TargetAttribute;
+%% Ends with
 is_custom_rule_match(?ENDS_WITH_OPERATOR, TargetAttribute, RuleValue) ->
-  Suffix = binary:part(TargetAttribute, {byte_size(TargetAttribute), -length(RuleValue)}),
+  Suffix = binary:part(TargetAttribute, {byte_size(TargetAttribute), -length(binary_to_list(RuleValue))}),
   string:equal(Suffix, RuleValue, false);
 is_custom_rule_match(_, _, <<>>) ->
   false.
