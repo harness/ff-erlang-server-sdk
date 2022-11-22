838486d94481bdbe01c54e9488f409cb058ec8e8
Erdi Rowlands <erdi.rowlands@harness.io>
2022-11-22 17:36:43 +0000

    FFM-5434 Move tests into test dir

---
+++ test/cfclient_ff_test_cases.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author erowlands
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 22. Nov 2022 17:33
+%%%-------------------------------------------------------------------
+-module(cfclient_ff_test_cases).
+-author("erowlands").
+
+-include_lib("eunit/include/eunit.hrl").
+
+simple_test() ->
+  ?assert(true).
