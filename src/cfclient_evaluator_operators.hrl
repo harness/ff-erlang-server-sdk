9c9d3daa986dd65e0a0a1c3589ed2c09529ead11
Jake Morrison <jake@cogini.com>
2023-01-19 12:26:33 -0600

    Format

--- src/cfclient_evaluator_operators.hrl
+++ src/cfclient_evaluator_operators.hrl
@@ -1,6 +1,9 @@
 %% Group Rule Association Operator - camel case from ff-server
+
 -define(SEGMENT_MATCH_OPERATOR, <<"segmentMatch">>).
+
 %% Custom Rule Operators
+
 -define(EQUAL_OPERATOR, <<"equal">>).
 -define(EQUAL_SENSITIVE_OPERATOR, <<"equal_sensitive">>).
 -define(STARTS_WITH_OPERATOR, <<"starts_with">>).
