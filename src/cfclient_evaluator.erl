f8b3295f1417e78cde7cb1c98dd800e3ab28b64d
Jake Morrison <jake@cogini.com>
2023-01-19 14:30:34 -0600

    Format

--- src/cfclient_evaluator.erl
+++ src/cfclient_evaluator.erl
@@ -318,6 +318,8 @@ is_custom_rule_match(?ENDS_WITH_OPERATOR, TargetAttribute, RuleValue) ->
 %% Contains
 is_custom_rule_match(?CONTAINS_OPERATOR, TargetAttribute, RuleValue) ->
   binary:match(TargetAttribute, hd(RuleValue)) /= nomatch;
+
+
 %% In - we don't get the head of RuleValue here as `In` can have multiple values
 is_custom_rule_match(?IN_OPERATOR, TargetAttribute, RuleValue) when is_binary(TargetAttribute) ->
   lists:member(TargetAttribute, RuleValue);
