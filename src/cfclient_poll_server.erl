ae095a7248ade39fd143d006752ceba0b79d40be
Jake Morrison <jake@cogini.com>
2023-01-19 12:10:26 -0600

    Format

--- src/cfclient_poll_server.erl
+++ src/cfclient_poll_server.erl
@@ -3,20 +3,19 @@
 %%% @doc
 %%% @end
 %%%-------------------------------------------------------------------
+
 -module(cfclient_poll_server).
 
 -behaviour(gen_server).
 
 -export([start_link/0]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
-  code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
 
 -record(cfclient_poll_server_state, {}).
 
-start_link() ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link() -> gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 init([]) ->
   logger:info("Starting poll server"),
@@ -26,11 +25,10 @@ init([]) ->
   erlang:send_after(PollInterval, self(), trigger),
   {ok, #cfclient_poll_server_state{}}.
 
-handle_call(_Request, _From, State = #cfclient_poll_server_state{}) ->
-  {reply, ok, State}.
 
-handle_cast(_Request, State = #cfclient_poll_server_state{}) ->
-  {noreply, State}.
+handle_call(_Request, _From, State = #cfclient_poll_server_state{}) -> {reply, ok, State}.
+
+handle_cast(_Request, State = #cfclient_poll_server_state{}) -> {noreply, State}.
 
 handle_info(_Info, State = #cfclient_poll_server_state{}) ->
   logger:info("Triggering poll server"),
@@ -40,8 +38,7 @@ handle_info(_Info, State = #cfclient_poll_server_state{}) ->
   erlang:send_after(PollInterval, self(), trigger),
   {noreply, State}.
 
-terminate(_Reason, _State = #cfclient_poll_server_state{}) ->
-  ok.
 
-code_change(_OldVsn, State = #cfclient_poll_server_state{}, _Extra) ->
-  {ok, State}.
+terminate(_Reason, _State = #cfclient_poll_server_state{}) -> ok.
+
+code_change(_OldVsn, State = #cfclient_poll_server_state{}, _Extra) -> {ok, State}.
