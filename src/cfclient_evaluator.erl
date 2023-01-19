767f88ac6f197284f9fac5cd5741d4522f1a44db
Jake Morrison <jake@cogini.com>
2023-01-19 15:12:20 -0600

    Match to get var

--- src/cfclient_evaluator.erl
+++ src/cfclient_evaluator.erl
@@ -242,7 +242,7 @@ is_rule_included_or_excluded([Head | Tail], Target) ->
   case maps:get(op, Head, false) of
     ?SEGMENT_MATCH_OPERATOR ->
       % At present there is only ever one element in values, so get the first one.
-      GroupName = hd(maps:get(values, Head, false)),
+      #{values := [GroupName | _Rest]} = Head,
       CachePid = cfclient_cache_repository:get_pid(),
       Group = cfclient_cache_repository:get_from_cache({segment, GroupName}, CachePid),
       search_group(excluded, Target, Group);
