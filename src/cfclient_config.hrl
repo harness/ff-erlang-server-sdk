9788506e7aa91752fd2f52bc9728476c6f22d3cf
Jake Morrison <jake@cogini.com>
2023-01-19 11:54:48 -0600

    Add shared vars

---
+++ src/cfclient_config.hrl
@@ -0,0 +1,5 @@
+% Shared config
+-define(CONFIG_TABLE, cfclient_config).
+-define(CACHE_TABLE, cfclient_cache).
+-define(METRICS_TARGET_CACHE_TABLE, cfclient_metrics_target_cache).
+-define(METRICS_CACHE_TABLE, cfclient_metrics_cache).
