4dd4ba79ebf745092d3ba1492b02d42c835d08b7
Jake Morrison <jake@cogini.com>
2023-01-20 14:16:54 -0600

    Use simpler name

--- src/cfclient_config.hrl
+++ src/cfclient_config.hrl
@@ -1,5 +1,6 @@
 % Shared config
 -define(CONFIG_TABLE, cfclient_config).
 -define(CACHE_TABLE, cfclient_cache).
--define(METRICS_TARGET_CACHE_TABLE, cfclient_metrics_target_cache).
+-define(METRICS_TARGET_TABLE, cfclient_metrics_target).
 -define(METRICS_CACHE_TABLE, cfclient_metrics_cache).
+-define(METRICS_COUNTER_TABLE, cfclient_metrics_counter).
