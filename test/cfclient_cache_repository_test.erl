5b1e7cee9c1df0a6a9baf63704d488327dd6736f
Jake Morrison <jake@cogini.com>
2023-01-20 13:44:47 -0600

    Ignore cache pid var in mocks

--- test/cfclient_cache_repository_test.erl
+++ test/cfclient_cache_repository_test.erl
@@ -7,25 +7,25 @@ get_from_cache_test() ->
   %% Mock LRU Cache
   meck:new(lru),
   %% Flag is found in cache
-  meck:expect(lru, get, fun (PID, <<"flags/does_exist">>) -> <<"flags/does_exist">> end),
+  meck:expect(lru, get, fun (_, <<"flags/does_exist">>) -> <<"flags/does_exist">> end),
   ?assertEqual(
     <<"flags/does_exist">>,
     cfclient_cache_repository:get_from_cache({flag, <<"does_exist">>}, PID)
   ),
   %% Flag is not found in cache
-  meck:expect(lru, get, fun (PID, <<"flags/does_not_exist">>) -> undefined end),
+  meck:expect(lru, get, fun (_, <<"flags/does_not_exist">>) -> undefined end),
   ?assertEqual(
     undefined,
     cfclient_cache_repository:get_from_cache({flag, <<"does_not_exist">>}, PID)
   ),
   %% Segment is found in cache
-  meck:expect(lru, get, fun (PID, <<"segments/does_exist">>) -> <<"segments/does_exist">> end),
+  meck:expect(lru, get, fun (_, <<"segments/does_exist">>) -> <<"segments/does_exist">> end),
   ?assertEqual(
     <<"segments/does_exist">>,
     cfclient_cache_repository:get_from_cache({segment, <<"does_exist">>}, PID)
   ),
   %% Segment is not found in cache
-  meck:expect(lru, get, fun (PID, <<"segments/does_not_exist">>) -> undefined end),
+  meck:expect(lru, get, fun (_, <<"segments/does_not_exist">>) -> undefined end),
   ?assertEqual(
     undefined,
     cfclient_cache_repository:get_from_cache({segment, <<"does_not_exist">>}, PID)
