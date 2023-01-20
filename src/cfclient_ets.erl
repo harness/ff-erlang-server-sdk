6aaabff00209e87f637eb4803130efe35e44e953
Jake Morrison <jake@cogini.com>
2023-01-20 13:37:03 -0600

    Make mocking of ets easier

---
+++ src/cfclient_ets.erl
@@ -0,0 +1,15 @@
+-module(cfclient_ets).
+
+-export([lookup/2, get/2]).
+
+% Used to support mocking
+-spec lookup(atom(), term()) -> list().
+lookup(Table, Key) -> ets:lookup(Table, Key).
+
+% Return values the same way as lru:get
+-spec get(atom(), binary()) -> term().
+get(Table, Key) ->
+  case ets:lookup(Table, Key) of
+    [] -> undefined;
+    [{Key, Value}] -> Value
+  end.
