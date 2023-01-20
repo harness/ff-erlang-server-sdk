2b8f930303c5f8404d8d5852280c20a1f1c557fc
Jake Morrison <jake@cogini.com>
2023-01-20 14:14:27 -0600

    Don't log in library

--- src/cfclient_retrieve.erl
+++ src/cfclient_retrieve.erl
@@ -6,30 +6,26 @@
 
 -module(cfclient_retrieve).
 
--include_lib("kernel/include/logger.hrl").
-
 -export([retrieve_flags/3, retrieve_segments/3]).
 
 % @doc Retrieve all features from FF API.
--spec retrieve_flags(ctx:t(), binary(), map()) -> {ok, list()} | {error, Reason :: atom()}.
+-spec retrieve_flags(ctx:t(), binary(), map()) -> {ok, list()} | {error, Reason :: term()}.
 retrieve_flags(Ctx, Environment, Opts) ->
   case cfapi_client_api:get_feature_config(Ctx, Environment, Opts) of
     {ok, Values, _} ->
       {ok, Values};
 
-    {error, Response, _} ->
-      ?LOG_ERROR("Error retrieving flags: ~p", [Response]),
-      {error, api}
+    {error, Reason, _} ->
+      {error, Reason}
   end.
 
 % @doc Retrieve all segments from FF API.
--spec retrieve_segments(ctx:t(), binary(), map()) -> {ok, [cfapi_segment:cfapi_segment()]} | {error, Reason :: atom()}.
+-spec retrieve_segments(ctx:t(), binary(), map()) -> {ok, [cfapi_segment:cfapi_segment()]} | {error, Reason :: term()}.
 retrieve_segments(Ctx, Environment, Opts) ->
   case cfapi_client_api:get_all_segments(Ctx, Environment, Opts) of
     {ok, Values, _} ->
       {ok, Values};
 
-    {error, Response, _} ->
-      ?LOG_ERROR("Error retrieving segments: ~p", [Response]),
-      {error, api}
+    {error, Reason, _} ->
+      {error, Reason}
   end.
