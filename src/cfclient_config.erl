d1cdfdcf3be570075a5e206f1cde9f74a1f2c395
Jake Morrison <jake@cogini.com>
2023-01-20 20:27:47 -0600

    New config

--- src/cfclient_config.erl
+++ src/cfclient_config.erl
@@ -38,141 +38,118 @@
 
 % Boolean for enabling analytics send to CF Server
 -define(DEFAULT_ANALYTICS_ENABLED, true).
-
 -define(DEFAULT_ANALYTICS_PUSH_INTERVAL, 60000).
 
 -spec init(string(), map() | list()) -> ok.
+init(ApiKey, Opts) when is_list(ApiKey), is_list(Opts) -> init(ApiKey, maps:from_list(Opts));
+
 init(ApiKey, Opts) when is_list(ApiKey), is_map(Opts) ->
-    Config = parse_options(ApiKey, Opts),
-    application:set_env(cfclient, config, Config).
+  Config = normalize_config(maps:merge(defaults(), Opts)),
+  application:set_env(cfclient, config, Config).
 
--spec parse_options(ApiKey :: string(), Opts :: map()) -> map().
-parse_options(ApiKey, Opts) when is_list(ApiKey), is_map(Opts) ->
-    ConfigUrl = parse_config_url(Opts),
-    EventsUrl = parse_events_url(Opts),
-    ConnectionTimeout = parse_connection_timeout(Opts),
-    ReadTimeout = parse_read_timeout(Opts),
-    WriteTimeout = parse_write_timeout(Opts),
-    PollInterval = parse_poll_interval(Opts),
-    StreamEnabled = parse_stream_enabled(Opts),
-    AnalyticsEnabled = parse_analytics_enabled(Opts),
-    AnalyticsPushInterval = parse_analytics_push_interval(Opts),
-    #{
-        api_key => ApiKey,
-        config_url => ConfigUrl,
-        events_url => EventsUrl,
-        connection_timeout => ConnectionTimeout,
-        read_timeout => ReadTimeout,
-        write_timeout => WriteTimeout,
-        poll_interval => PollInterval,
-        stream_enabled => StreamEnabled,
-        analytics_enabled => AnalyticsEnabled,
-        analytics_push_interval => AnalyticsPushInterval
-    }.
 
--spec get_value(Key :: atom() | string()) -> string() | term().
-get_value(Key) when is_list(Key) ->
-    get_value(list_to_atom(Key));
+-spec get_config() -> {ok, map()} | {error, Reason :: atom()}.
+get_config() -> get_config(default).
+
+-spec get_config(atom()) -> {ok, map()} | {error, Reason :: atom()}.
+get_config(Project) ->
+  case ets:lookup(?CONFIG_TABLE, Project) of
+    [] -> {error, undefined};
+    [{Project, Value}] -> {ok, Value}
+  end.
+
+
+-spec defaults() -> map().
+defaults() ->
+  #{
+    name => default,
+    % Config endpoint for prod
+    config_url => ?DEFAULT_CONFIG_URL,
+    % Event endpoint for prod
+    events_url => ?DEFAULT_EVENTS_URL,
+    % Timeout in milliseconds
+    connection_timeout => ?DEFAULT_CONNECTION_TIMEOUT,
+    % Timeout reading data from CF Server, in milliseconds
+    read_timeout => ?DEFAULT_READ_TIMEOUT,
+    % Timout writing data to CF Server, in milliseconds
+    write_timeout => ?DEFAULT_WRITE_TIMEOUT,
+    % Polling data from CF Server, in milliseconds
+    poll_interval => ?DEFAULT_POLL_INTERVAL,
+    % Enable events stream
+    stream_enabled => ?DEFAULT_STREAM_ENABLED,
+    % Enable sending analytics to CF Server
+    analytics_enabled => ?DEFAULT_ANALYTICS_ENABLED,
+    analytics_push_interval => ?DEFAULT_ANALYTICS_PUSH_INTERVAL,
+    % ETS table for configuration
+    config_table => ?CONFIG_TABLE,
+    cache_table => ?CACHE_TABLE,
+    target_table => ?METRICS_TARGET_TABLE,
+    metrics_cache_table => ?METRICS_CACHE_TABLE,
+    metrics_counter_table => ?METRICS_COUNTER_TABLE
+  }.
+
+
+-spec normalize(proplists:proplist()) -> map().
+normalize(Config0) ->
+  Config1 = maps:from_list(Config0),
+  Config2 = normalize_config(maps:merge(defaults(), Config1)),
+  case maps:get(name, Config2) of
+    default -> Config2;
+
+    Name ->
+      % Add name prefix to tables
+      Tables =
+        [config_table, cache_table, target_table, metrics_cache_table, metrics_counter_table],
+      Prefixed = maps:map(fun (_K, V) -> add_prefix(Name, V) end, maps:with(Tables, Config2)),
+      maps:merge(Config2, Prefixed)
+  end.
+
+
+add_prefix(Name, Table) -> list_to_atom(atom_to_list(Name) ++ "_" ++ atom_to_list(Table)).
+
+-spec normalize_config(map()) -> map().
+normalize_config(Config) -> maps:fold(fun normalize_config/3, #{}, Config).
+
+normalize_config(poll_interval = K, V, Acc) when is_integer(V), V < 60000 ->
+  ?LOG_WARNING("~s must be at least 60 sec, using default ~w", [K, ?DEFAULT_POLL_INTERVAL]),
+  maps:put(K, ?DEFAULT_POLL_INTERVAL, Acc);
+
+normalize_config(push_interval = K, V, Acc) when is_integer(V), V < 60000 ->
+  ?LOG_WARNING(
+    "~s must be at least 60 sec, using default ~w",
+    [K, ?DEFAULT_ANALYTICS_PUSH_INTERVAL]
+  ),
+  maps:put(K, ?DEFAULT_ANALYTICS_PUSH_INTERVAL, Acc);
+
+normalize_config(config_url = K, V, Acc) -> maps:put(K, normalize_url(V), Acc);
+normalize_config(events_url = K, V, Acc) -> maps:put(K, normalize_url(V), Acc);
+normalize_config(K, V, Acc) -> maps:put(K, V, Acc).
+
+
+% Strip trailing / from URL
+normalize_url(V) -> string:trim(V, trailing, "/").
+
+% TODO: validate the JWT
+-spec parse_jwt(binary()) -> {ok, map()} | {error, Reason :: term()}.
+parse_jwt(JwtToken) ->
+  JwtString = lists:nth(2, binary:split(JwtToken, <<".">>, [global])),
+  DecodedJwt = base64url:decode(JwtString),
+  case unicode:characters_to_binary(DecodedJwt, utf8) of
+    UnicodeJwt when is_binary(UnicodeJwt) ->
+      Result = jsx:decode(string:trim(UnicodeJwt), [attempt_atom]),
+      {ok, Result};
+
+    _ -> {error, unicode}
+  end.
+
+
+-spec get_value(atom() | string()) -> string() | term().
+get_value(Key) when is_list(Key) -> get_value(list_to_atom(Key));
+
 get_value(Key) when is_atom(Key) ->
-    {ok, Config} = application:get_env(cfclient, config),
-    maps:get(Key, Config).
+  {ok, Config} = application:get_env(cfclient, config),
+  maps:get(Key, Config).
 
-parse_stream_enabled(Opts) ->
-    case maps:get(stream_enabled, Opts, not_found) of
-        not_found ->
-            ?LOG_DEBUG("stream_enabled key not provided in client config, using default value: ~p~n", [?DEFAULT_STREAM_ENABLED]),
-            ?DEFAULT_STREAM_ENABLED;
-        StreamEnabled ->
-            ?LOG_DEBUG("stream_enabled is: ~p~n", [StreamEnabled]),
-            StreamEnabled
-    end.
-
-parse_poll_interval(Opts) ->
-    case maps:get(poll_interval, Opts, not_found) of
-        not_found ->
-            ?LOG_DEBUG("poll_interval key not provided in client config, using default value: ~p~n", [?DEFAULT_POLL_INTERVAL]),
-            ?DEFAULT_POLL_INTERVAL;
-        PollInterval when PollInterval < 60000 ->
-            ?LOG_ERROR("poll_interval is ~p seconds but must not be lower than 60 seconds. Using default value ~pn: ", [?DEFAULT_POLL_INTERVAL]),
-            ?DEFAULT_POLL_INTERVAL;
-        PollInterval when PollInterval >= 60000 ->
-            ?LOG_DEBUG("poll_interval is: ~p~n", [PollInterval]),
-            PollInterval
-    end.
-
-parse_write_timeout(Opts) ->
-    case maps:get(write_timeout, Opts, not_found) of
-        not_found ->
-            ?LOG_DEBUG("write_timeout key not provided in client config, using default value: ~p~n", [?DEFAULT_WRITE_TIMEOUT]),
-            ?DEFAULT_WRITE_TIMEOUT;
-        WriteTimeout ->
-            ?LOG_DEBUG("write_timeout is: ~p~n", [WriteTimeout]),
-            WriteTimeout
-    end.
-
-parse_read_timeout(Opts) ->
-    case maps:get(read_timeout, Opts, not_found) of
-        not_found ->
-            ?LOG_DEBUG("read_timeout key not provided in client config, using default value: ~p~n", [?DEFAULT_READ_TIMEOUT]),
-            ?DEFAULT_READ_TIMEOUT;
-        ReadTimeout ->
-            ?LOG_DEBUG("read_timeout is: ~p~n", [ReadTimeout]),
-            ReadTimeout
-    end.
-
-parse_connection_timeout(Opts) ->
-    case maps:get(connection_timeout, Opts, not_found) of
-        not_found ->
-            ?LOG_DEBUG("connection_timeout key not provided in client config, using default value: ~p~n", [?DEFAULT_CONNECTION_TIMEOUT]),
-            ?DEFAULT_CONNECTION_TIMEOUT;
-        ConnectionTimeout ->
-            ?LOG_DEBUG("connection_timeout is: ~p~n", [ConnectionTimeout]),
-            ConnectionTimeout
-    end.
-
-parse_events_url(Opts) ->
-    case maps:get(events_url, Opts, not_found) of
-        not_found ->
-            ?LOG_DEBUG("events_url key not provided in client config, using default value: ~p~n", [?DEFAULT_EVENTS_URL]),
-            string:trim(?DEFAULT_EVENTS_URL, trailing, "/");
-        EventsUrl ->
-            ?LOG_DEBUG("events_url is: ~p~n", [EventsUrl]),
-            string:trim(EventsUrl, trailing, "/")
-    end.
-
-
-parse_config_url(Opts) ->
-    case maps:get(config_url, Opts, not_found) of
-        not_found ->
-            ?LOG_DEBUG("config_url key not provided in client config, using default value: ~p~n", [?DEFAULT_CONFIG_URL]),
-            string:trim(?DEFAULT_CONFIG_URL, trailing, "/");
-        ConfigUrl ->
-            ?LOG_DEBUG("config_url is: ~p~n", [ConfigUrl]),
-            string:trim(ConfigUrl, trailing, "/")
-    end.
-
-parse_analytics_enabled(Opts) ->
-    case maps:get(analytics_enabled, Opts, not_found) of
-        not_found ->
-            ?LOG_DEBUG("analytics_enabled key not provided in client config, using default value: ~p~n", [?DEFAULT_ANALYTICS_ENABLED]),
-            ?DEFAULT_ANALYTICS_ENABLED;
-        AnalyticsEnabled ->
-            ?LOG_DEBUG("analytics_enabled is: ~p~n", [AnalyticsEnabled]),
-            AnalyticsEnabled
-    end.
-
-parse_analytics_push_interval(Opts)  ->
-    case maps:get(analytics_push_interval, Opts, not_found) of
-        not_found ->
-            ?LOG_DEBUG("analytics_push_interval key not provided in client config, using default value: ~p~n", [?DEFAULT_ANALYTICS_PUSH_INTERVAL]),
-            ?DEFAULT_ANALYTICS_PUSH_INTERVAL;
-        PushInterval when PushInterval < 60000->
-            ?LOG_ERROR("Analytics push interval is ~p seconds but must not be lower than 60 seconds. Using default value ~pn: ", [?DEFAULT_ANALYTICS_PUSH_INTERVAL]),
-            ?DEFAULT_ANALYTICS_PUSH_INTERVAL;
-        PushInterval when PushInterval >= 60000 ->
-            ?LOG_DEBUG("analytics_push_interval is: ~p~n", [PushInterval]),
-            PushInterval
-    end.
 
 -spec clear_config() -> ok.
 clear_config() -> application:unset_env(cfclient, config).
