b1c6f0b74af4a05e71f38795d42de7e8797f722f
Jake Morrison <jake@cogini.com>
2023-01-20 20:21:23 -0600

    Format

--- src/cfclient_retrieve.erl
+++ src/cfclient_retrieve.erl
@@ -14,37 +14,30 @@
 % @doc Retrieve all features from FF API.
 -spec retrieve_flags(map()) -> {ok, [flag()]} | {error, Reason :: term()}.
 retrieve_flags(Config) ->
-    #{auth_token := AuthToken, project := Project, config_url := ConfigUrl} = Config,
-    #{environment := Env, clusterIdentifier := Cluster} = Project,
-    Opts =
+  #{auth_token := AuthToken, project := Project, config_url := ConfigUrl} = Config,
+  #{environment := Env, clusterIdentifier := Cluster} = Project,
+  Opts =
     #{
       cfg => #{auth => #{'BearerAuth' => <<"Bearer ", AuthToken/binary>>}, host => ConfigUrl},
       params => #{cluster => Cluster}
-     },
-
-    case cfapi_client_api:get_feature_config(Ctx, Env, Opts) of
-        {ok, Values, _} ->
-            {ok, Values};
-
-        {error, Reason, _} ->
-            {error, Reason}
-    end.
+    },
+  case cfapi_client_api:get_feature_config(ctx:new(), Env, Opts) of
+    {ok, Values, _} -> {ok, Values};
+    {error, Reason, _} -> {error, Reason}
+  end.
 
 
 % @doc Retrieve all segments from FF API.
 -spec retrieve_segments(map()) -> {ok, [segment()]} | {error, Reason :: term()}.
 retrieve_segments(Config) ->
-    #{auth_token := AuthToken, project := Project, config_url := ConfigUrl} = Config,
-    #{environment := Env, clusterIdentifier := Cluster} = Project,
-    Opts =
+  #{auth_token := AuthToken, project := Project, config_url := ConfigUrl} = Config,
+  #{environment := Env, clusterIdentifier := Cluster} = Project,
+  Opts =
     #{
       cfg => #{auth => #{'BearerAuth' => <<"Bearer ", AuthToken/binary>>}, host => ConfigUrl},
       params => #{cluster => Cluster}
-     },
-    case cfapi_client_api:get_all_segments(Ctx, Env, Opts) of
-        {ok, Values} ->
-            {ok, Values};
-
-        {error, Reason, _} ->
-            {error, Reason}
-    end.
+    },
+  case cfapi_client_api:get_all_segments(ctx:new(), Env, Opts) of
+    {ok, Values} -> {ok, Values};
+    {error, Reason, _} -> {error, Reason}
+  end.
