3c2ca8b68741cc1bf1fb89eaca3188ee1381278b
Jake Morrison <jake@cogini.com>
2023-01-20 20:06:00 -0600

    Remove obsolete tests

--- test/cfclient_cache_repository_test.erl
+++ test/cfclient_cache_repository_test.erl
@@ -3,34 +3,36 @@
 -include_lib("eunit/include/eunit.hrl").
 
 get_from_cache_test() ->
-  PID = self(),
-  %% Mock LRU Cache
-  meck:new(lru),
+  meck:new(cfclient_ets),
   %% Flag is found in cache
-  meck:expect(lru, get, fun (_, <<"flags/does_exist">>) -> <<"flags/does_exist">> end),
+  meck:expect(cfclient_ets, get, fun (_, <<"flags/does_exist">>) -> <<"flags/does_exist">> end),
   ?assertEqual(
-    <<"flags/does_exist">>,
-    cfclient_cache_repository:get_from_cache({flag, <<"does_exist">>}, PID)
+    {ok, <<"flags/does_exist">>},
+    cfclient_cache_repository:get_value({flag, <<"does_exist">>})
   ),
   %% Flag is not found in cache
-  meck:expect(lru, get, fun (_, <<"flags/does_not_exist">>) -> undefined end),
+  meck:expect(cfclient_ets, get, fun (_, <<"flags/does_not_exist">>) -> undefined end),
   ?assertEqual(
-    undefined,
-    cfclient_cache_repository:get_from_cache({flag, <<"does_not_exist">>}, PID)
+    {error, undefined},
+    cfclient_cache_repository:get_value({flag, <<"does_not_exist">>})
   ),
   %% Segment is found in cache
-  meck:expect(lru, get, fun (_, <<"segments/does_exist">>) -> <<"segments/does_exist">> end),
+  meck:expect(
+    cfclient_ets,
+    get,
+    fun (_, <<"segments/does_exist">>) -> <<"segments/does_exist">> end
+  ),
   ?assertEqual(
-    <<"segments/does_exist">>,
-    cfclient_cache_repository:get_from_cache({segment, <<"does_exist">>}, PID)
+    {ok, <<"segments/does_exist">>},
+    cfclient_cache_repository:get_value({segment, <<"does_exist">>})
   ),
   %% Segment is not found in cache
-  meck:expect(lru, get, fun (_, <<"segments/does_not_exist">>) -> undefined end),
+  meck:expect(cfclient_ets, get, fun (_, <<"segments/does_not_exist">>) -> undefined end),
   ?assertEqual(
-    undefined,
-    cfclient_cache_repository:get_from_cache({segment, <<"does_not_exist">>}, PID)
+    {error, undefined},
+    cfclient_cache_repository:get_value({segment, <<"does_not_exist">>})
   ),
-  meck:unload(lru).
+  meck:unload(cfclient_ets).
 
 
 format_key_test() ->
