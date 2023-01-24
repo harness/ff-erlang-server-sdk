eae9268a40bc37e6e2260f8e7e972cd8758b41a5
Jake Morrison <jake@cogini.com>
2023-01-24 08:23:12 -0600

    Set default key for tests

--- src/cfclient_app.erl
+++ src/cfclient_app.erl
@@ -10,7 +10,7 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-  {ok, ApiKey} = application:get_env(cfclient, api_key),
+  {ok, ApiKey} = application:get_env(cfclient, api_key, "fake_key"),
   {ok, Config} = application:get_env(cfclient, config, []),
   cfclient_sup:start_link([{api_key, ApiKey}, {config, Config}]).
 
