b3f42e61ba99e00702ee58e5afa6ec12efb1765d
Jake Morrison <jake.morrison@therealreal.com>
2023-01-26 11:17:43 -0600

    Clean up

--- src/cfclient_sup.erl
+++ src/cfclient_sup.erl
@@ -1,7 +1,5 @@
-%%%-------------------------------------------------------------------
-%% @doc cfclient top level supervisor.
-%% @end
-%%%-------------------------------------------------------------------
+% @doc cfclient top level supervisor.
+% @end
 
 -module(cfclient_sup).
 
@@ -19,22 +17,3 @@ init(Args) ->
   ChildSpecs = [#{id => cfclient_instance, start => {cfclient_instance, start_link, [Args]}}],
   SupFlags = #{strategy => one_for_one, intensity => 1, period => 5},
   {ok, {SupFlags, lists:flatten(ChildSpecs)}}.
-
-
-% @doc Start metrics gen_server and caches for metrics and metrics targets
-analytics_children(#{analytics_enabled := true}) ->
-  [
-    #{
-      id => cfclient_metrics_lru,
-      start => {lru, start_link, [[{max_size, 32000000}]]},
-      modules => [lru]
-    },
-    #{
-      id => cfclient_metrics_target_lru,
-      start => {lru, start_link, [[{max_size, 32000000}]]},
-      modules => [lru]
-    }
-    #{id => cfclient_metrics, start => {cfclient_metrics, start_link, []}}
-  ];
-
-analytics_children(_) -> [].
