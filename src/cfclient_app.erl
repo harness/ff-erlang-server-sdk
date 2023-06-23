a64b052e784bd4bdc793fd9dab20c59cc8691802
Erdi Rowlands <erdi.rowlands@harness.io>
2023-06-23 18:09:17 +0100

    FFM-8323 Fix the global logger's level from being set. Only set application level

--- src/cfclient_app.erl
+++ src/cfclient_app.erl
@@ -11,7 +11,7 @@
 start(_StartType, _StartArgs) ->
   ApiKey = application:get_env(cfclient, api_key, undefined),
   LogLevel = application:get_env(cfclient, log_level, warning),
-  logger:set_primary_config(level, LogLevel),
+  logger:set_application_level(cfclient, LogLevel),
   Config = application:get_env(cfclient, config, []),
   StartDefaultInstance = application:get_env(cfclient, start_default_instance, true),
   cfclient_sup:start_link([{api_key, ApiKey}, {config, Config}, {start_default_instance, StartDefaultInstance}]).
