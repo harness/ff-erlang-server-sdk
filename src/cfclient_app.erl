20d686a622fd4b94e7239efb69bd1ce9fb80df67
Erdi Rowlands <erdi.rowlands@harness.io>
2023-06-29 19:48:39 +0100

    FFM-8300 Set evaluation module level

--- src/cfclient_app.erl
+++ src/cfclient_app.erl
@@ -11,12 +11,15 @@
 start(_StartType, _StartArgs) ->
   ApiKey = application:get_env(cfclient, api_key, undefined),
   LogLevel = application:get_env(cfclient, log_level, warning),
-  logger:set_application_level(cfclient, LogLevel),
-  Config = application:get_env(cfclient, config, []),
-  case maps:get(verbose_evaluation_logs, Config) of
+  ok = logger:set_application_level(cfclient, LogLevel),
+  ok = case application:get_env(cfclient, verbose_evaluation_logs, false) of
     true ->
-      logger:set_module_level(cfclient_evaluator, info)
+      logger:set_module_level(cfclient_evaluator, info);
+    false ->
+      ok
   end,
+  Config = application:get_env(cfclient, config, []),
+
     StartDefaultInstance = application:get_env(cfclient, start_default_instance, true),
   cfclient_sup:start_link([{api_key, ApiKey}, {config, Config}, {start_default_instance, StartDefaultInstance}]).
 
