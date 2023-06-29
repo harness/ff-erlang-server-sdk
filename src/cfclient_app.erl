3d488091971caebe320eb531b1a757b4e0f98d8a
Erdi Rowlands <erdi.rowlands@harness.io>
2023-06-29 20:01:57 +0100

    FFM-8364 Fix verbose_evaluation_logs to work at `warning` level and above (#53)
    * FFM-8300 Set evaluation module level
    
    * FFM-8300 Set evaluation module level
    
    * FFM-8300 Set evaluation module level
    
    * FFM-8300 1.2.1 release prep
    
    * FFM-8300 1.2.1 release prep

--- src/cfclient_app.erl
+++ src/cfclient_app.erl
@@ -11,10 +11,19 @@
 start(_StartType, _StartArgs) ->
   ApiKey = application:get_env(cfclient, api_key, undefined),
   LogLevel = application:get_env(cfclient, log_level, warning),
-  logger:set_application_level(cfclient, LogLevel),
+  ok = logger:set_application_level(cfclient, LogLevel),
   Config = application:get_env(cfclient, config, []),
+  Config2 =
+    case application:get_env(cfclient, verbose_evaluation_logs, false) of
+      true ->
+        logger:set_module_level(cfclient_evaluator, info),
+        [{verbose_evaluation_logs, true}] ++ Config;
+      false ->
+        Config
+    end,
+
   StartDefaultInstance = application:get_env(cfclient, start_default_instance, true),
-  cfclient_sup:start_link([{api_key, ApiKey}, {config, Config}, {start_default_instance, StartDefaultInstance}]).
+  cfclient_sup:start_link([{api_key, ApiKey}, {config, Config2}, {start_default_instance, StartDefaultInstance}]).
 
 
 stop(_State) -> ok.
