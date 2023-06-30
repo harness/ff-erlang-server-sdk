445804e520d2e8b54251f87b08edb74275fd5896
Erdi Rowlands <erdi.rowlands@harness.io>
2023-06-30 06:26:53 +0100

    FFM-8300 Add new murmur3 lib

--- src/cfclient_evaluator.erl
+++ src/cfclient_evaluator.erl
@@ -493,9 +493,7 @@ apply_percentage_rollout([], _, _, _) -> excluded.
 
 -spec should_rollout(binary(), binary(), integer()) -> boolean().
 should_rollout(BucketBy, TargetValue, Percentage) ->
-  Hash = excludedrlang_murmurhash:murmurhash3_32(<<TargetValue/binary, ":", BucketBy/binary>>),
-  murmur:hash_x86_32("b2622f5e1310a0aa14b7f957fe4246fa", 2147368987),
-  BucketID = (Hash rem 100) + 1,
+  BucketID = (2 rem 100) + 1,
   (Percentage > 0) andalso (BucketID =< Percentage).
 
 
