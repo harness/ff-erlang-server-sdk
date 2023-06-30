6ae93d9ebf3c4facf2bcb5d89dc225658661d1ad
Erdi Rowlands <erdi.rowlands@harness.io>
2023-06-30 08:22:45 +0100

    FFM-8300 comment

--- src/cfclient_evaluator.erl
+++ src/cfclient_evaluator.erl
@@ -493,8 +493,8 @@ apply_percentage_rollout([], _, _, _) -> excluded.
 
 -spec should_rollout(binary(), binary(), integer()) -> boolean().
 should_rollout(BucketBy, TargetValue, Percentage) ->
-%%  Hash = erlang_murmurhash:murmurhash3_32(<<TargetValue/binary,":",BucketBy/binary>>),
   Concatenated = <<TargetValue/binary, ":", BucketBy/binary>>,
+  % Using a pure Elixir library for murmur3 
   Hash = 'Elixir.Murmur':hash_x86_32(Concatenated),
   BucketID = (Hash rem 100) + 1,
   (Percentage > 0) andalso (BucketID =< Percentage).
